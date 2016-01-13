515f6cc6f48f39a48fad2c9becc3dff31285f905
Maitane Chaves <maitane.chaves@gmail.com>
2016-01-13 16:33:37 +0100

    Añadidas clases Parejas, Huecos y Frases del modelo, ProgressTask y ModelActivity

---
+++ app/src/main/java/es/tta/abuapp/model/Parejas.java
@@ -0,0 +1,184 @@
+package es.tta.abuapp.model;
+
+import java.util.HashMap;
+
+public class Parejas {
+    private String palabra1;
+    private String palabra2;
+    private String palabra3;
+    private String palabra4;
+    private String palabra5;
+    private String palabra6;
+
+    private String imagen1;
+    private String imagen2;
+    private String imagen3;
+    private String imagen4;
+    private String imagen5;
+    private String imagen6;
+
+    private String comp1;
+    private String comp2;
+    private String comp3;
+    private String comp4;
+    private String comp5;
+    private String comp6;
+
+    private int next_pareja;
+
+    public int getNext_pareja() {
+        return next_pareja;
+    }
+
+    public void setNext_pareja(int next_pareja) {
+        this.next_pareja = next_pareja;
+    }
+
+    public String getPalabra1() {
+        return palabra1;
+    }
+
+    public void setPalabra1(String palabra1) {
+        this.palabra1 = palabra1;
+    }
+
+    public String getPalabra2() {
+        return palabra2;
+    }
+
+    public void setPalabra2(String palabra2) {
+        this.palabra2 = palabra2;
+    }
+
+    public String getPalabra3() {
+        return palabra3;
+    }
+
+    public void setPalabra3(String palabra3) {
+        this.palabra3 = palabra3;
+    }
+
+    public String getPalabra4() {
+        return palabra4;
+    }
+
+    public void setPalabra4(String palabra4) {
+        this.palabra4 = palabra4;
+    }
+
+    public String getPalabra5() {
+        return palabra5;
+    }
+
+    public void setPalabra5(String palabra5) {
+        this.palabra5 = palabra5;
+    }
+
+    public String getPalabra6() {
+        return palabra6;
+    }
+
+    public void setPalabra6(String palabra6) {
+        this.palabra6 = palabra6;
+    }
+
+    public String getImagen1() {
+        return imagen1;
+    }
+
+    public void setImagen1(String imagen1) {
+        this.imagen1 = imagen1;
+    }
+
+    public String getImagen2() {
+        return imagen2;
+    }
+
+    public void setImagen2(String imagen2) {
+        this.imagen2 = imagen2;
+    }
+
+    public String getImagen3() {
+        return imagen3;
+    }
+
+    public void setImagen3(String imagen3) {
+        this.imagen3 = imagen3;
+    }
+
+    public String getImagen4() {
+        return imagen4;
+    }
+
+    public void setImagen4(String imagen4) {
+        this.imagen4 = imagen4;
+    }
+
+    public String getImagen5() {
+        return imagen5;
+    }
+
+    public void setImagen5(String imagen5) {
+        this.imagen5 = imagen5;
+    }
+
+    public String getImagen6() {
+        return imagen6;
+    }
+
+    public void setImagen6(String imagen6) {
+        this.imagen6 = imagen6;
+    }
+
+    public String getComp1() {
+        return comp1;
+    }
+
+    public void setComp1(String comp1) {
+        this.comp1 = comp1;
+    }
+
+    public String getComp2() {
+        return comp2;
+    }
+
+    public void setComp2(String comp2) {
+        this.comp2 = comp2;
+    }
+
+    public String getComp3() {
+        return comp3;
+    }
+
+    public void setComp3(String comp3) {
+        this.comp3 = comp3;
+    }
+
+    public String getComp4() {
+        return comp4;
+    }
+
+    public void setComp4(String comp4) {
+        this.comp4 = comp4;
+    }
+
+    public String getComp5() {
+        return comp5;
+    }
+
+    public void setComp5(String comp5) {
+        this.comp5 = comp5;
+    }
+
+    public String getComp6() {
+        return comp6;
+    }
+
+    public void setComp6(String comp6) {
+        this.comp6 = comp6;
+    }
+
+
+
+
+}
