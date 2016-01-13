1aaecb40667f5c6e249ff022d727f333b97e95d7
Naiara Garcia <ngarcia091@ikasle.ehu.eus>
2016-01-13 16:37:39 +0100

    Añadidas clases Canciones, Audios y Business del modelo

---
+++ app/src/main/java/es/tta/abuapp/Business.java
@@ -0,0 +1,35 @@
+package es.tta.abuapp;
+
+
+public class Business
+{
+
+    public void getParejas()
+    {
+
+    }
+
+    public void getHuecos()
+    {
+
+    }
+
+    public void getFrases()
+    {
+
+    }
+
+    public void getCanciones()
+    {
+
+    }
+
+    public void getAudios()
+    {
+
+    }
+}
+
+
+
+
---
+++ app/src/main/java/es/tta/abuapp/Canciones.java
@@ -0,0 +1,32 @@
+package es.tta.abuapp;
+
+public class Canciones
+{
+    private String titulo;
+    private String video;
+    private int nextCanciones;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getVideo() {
+        return video;
+    }
+
+    public void setVideo(String video) {
+        this.video = video;
+    }
+
+    public int getNextCanciones() {
+        return nextCanciones;
+    }
+
+    public void setNextCanciones(int nextCanciones) {
+        this.nextCanciones = nextCanciones;
+    }
+}
