1115b5702ae2e5140ae62b0b9cbb45312daa9f68
Naiara Garcia <ngarcia091@ikasle.ehu.eus>
2016-01-24 21:56:03 +0100

    Creada clase BusinessFrases y rellenadas clases DiaADiaActivity y FrasesDiaADiaActivity antes vacías para comunicación con servidor para mostrar todas las frases según el lugar escogido (bar, médico o pan)

--- app/src/main/java/es/tta/abuapp/DiaADiaActivity.java
+++ app/src/main/java/es/tta/abuapp/DiaADiaActivity.java
@@ -7,8 +7,11 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 public class DiaADiaActivity extends AppCompatActivity {
+
+    public final static String EXTRA_TIPO = "es.tta.ejemplo1.tipo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +19,24 @@ public class DiaADiaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_dia_adia);
     }
 
-    public void mostrarFrases(View view)
+    public void mostrarFrasesPan(View view)
     {
         Intent intent = new Intent(this,FrasesDiaADiaActivity.class);
+        intent.putExtra(EXTRA_TIPO, "pan");
+        startActivity(intent);
+    }
+
+    public void mostrarFrasesMedico(View view)
+    {
+        Intent intent = new Intent(this,FrasesDiaADiaActivity.class);
+        intent.putExtra(EXTRA_TIPO, "medico");
+        startActivity(intent);
+    }
+
+    public void mostrarFrasesBar(View view)
+    {
+        Intent intent = new Intent(this,FrasesDiaADiaActivity.class);
+        intent.putExtra(EXTRA_TIPO, "bar");
         startActivity(intent);
     }
 
--- app/src/main/java/es/tta/abuapp/FrasesDiaADiaActivity.java
+++ app/src/main/java/es/tta/abuapp/FrasesDiaADiaActivity.java
@@ -1,18 +1,69 @@
 package es.tta.abuapp;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import es.tta.abuapp.model.BusinessFrases;
+import es.tta.abuapp.model.Frases;
 
 public class FrasesDiaADiaActivity extends AppCompatActivity {
+
+    public static final String URL = "http://vps213926.ovh.net/AbuApp";
+    private Client php= new Client(URL);
+    private BusinessFrases server;
+    private Frases frase;
+    private String[] frases;
+    //private DataFrases data;
+
+    private String tipo;
+    private TextView tituloView;
+    private LinearLayout layout;
+    private int num_pag=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frases_dia_adia);
+
+        Intent intent = getIntent();
+        tipo = intent.getStringExtra(DiaADiaActivity.EXTRA_TIPO);
+        tituloView = (TextView)findViewById(R.id.titulo_diaadia);
+        tituloView.setText(tipo);
+        layout = (LinearLayout)findViewById(R.id.frases_layout);
+
+        php = new Client(URL);
+        server = new BusinessFrases(php);
+
+        frases = new String[3]; //CAMBIAR POR MAX_FRASES
+        mostrarFrasesTodas();
     }
 
+    public void mostrarFrasesTodas()
+    {
+        new ProgressTask<String[]>(this)
+        {
+            @Override
+            protected String[] work() throws Exception {
+                for(int i=1;i<=3/*server.getMAX_FRASES()*/;i++)
+                {
+                    frase = server.getFrases(num_pag, tipo);
+                    frases[num_pag-1] = frase.getFrase_castellano();
+                    num_pag++;
+                }
+                return frases;
+            }
+
+            @Override
+            protected void onFinish(String[] frases) {
+                for(int i=1;i<=3/*server.getMAX_FRASES()*/;i++)
+                {
+                    TextView f = new TextView(FrasesDiaADiaActivity.this);
+                    f.setText(frases[i-1]);
+                    layout.addView(f);
+                }
+            }
+        }.execute();
+    }
 }
