b847d19028f139038992c1e2049a63bb37fcbb59
Maitane Chaves <maitane.chaves@gmail.com>
2016-01-26 18:24:39 +0100

    Solucion

--- app/src/main/java/es/tta/abuapp/JuegosActivity.java
+++ app/src/main/java/es/tta/abuapp/JuegosActivity.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import es.tta.abuapp.presentador_vista.ParejasActivity;
-
 public class JuegosActivity extends AppCompatActivity {
 
     @Override
